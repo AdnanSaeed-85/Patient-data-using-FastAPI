fcdb07af9cbe46eaa539c7347eab4600085c7090
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-03-24 21:34:54 +0500

    successfully created new end point (creat) using post

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
 import json
+from pydantic import Field, BaseModel, computed_field
+from typing import Annotated, Literal
 
 app = FastAPI()
+
+class data_validator(BaseModel):
+    id: Annotated[str, Field(..., description="ID of the patient", examples=['P001'])]
+    name: Annotated[str, Field(..., description="Name of the patient")]
+    age: Annotated[int, Field(..., description="Age of the patient", ge=0, le=120)]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description="Gender of the patient")]
+    city: Annotated[str, Field(..., description="City where patient belong")]
+    height: Annotated[float, Field(..., description="Height of the patient in meters", ge=0)]
+    weight: Annotated[float, Field(..., description="Weight of the patient in kgs", ge=0)]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi_cal = round(self.weight/(self.height**2), 2)
+        return bmi_cal
+
+    @computed_field
+    @property
+    def verdict_cal(self) -> str:
+        if self.bmi < 18.5:
+            return 'underweight'
+        elif self.bmi >= 18.5 and self.bmi < 24.9:
+            return 'Normal'
+        elif self.bmi >= 25 and self.bmi < 29.9:
+            return 'overweight'
+        if self.bmi >= 30:
+            return 'obese'
+        
 
 def data_loader():
     with open('patient_data.json', 'r') as f:
         data = json.load(f)
         return data
+
+def save_data(data):
+    with open('patient_data.json', 'w') as f:
+        json.dump(data, f)
 
 @app.get('/')
 def title():
@@ -43,9 +78,19 @@ def sort_fun(sort_by: str = Query(..., description="sort your data on the basis 
         raise HTTPException(status_code=400, detail=f"Invalid order selection, Chose from {valid_ordering}")
     
     data = data_loader()
-
     ordering = True if order == 'asc' else False
-
     sorted_data = sorted(data.values(), key=lambda x: x[sort_by.lower()], reverse=ordering)
-
     return sorted_data
+
+@app.post('/create')
+def create_patient(patient: data_validator):
+    data = data_loader()
+
+    if patient.id in data.keys():
+        raise HTTPException(status_code=400, detail='Patient already exists')
+    
+    data[patient.id] = patient.model_dump(exclude=['id'])
+
+    save_data(data)
+
+    return JSONResponse(status_code=201, content={'message': 'Patient created successfully'})
